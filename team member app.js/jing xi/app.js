b150b71a2b24ca540271b974597c5ced921b0197
jingxikoh <24011503@myrp.edu.sg>
2025-07-24 19:44:01 +0800

    app.js delete

---
+++ team member app.js/jing xi/app.js
@@ -0,0 +1,15 @@
+app.get('/deleteItem/:id', (req, res) => {
+    const itemId = req.params.id
+    const sql = 'DELETE FROM inventory WHERE itemId = ?';
+    connection.query( sql, [itemId], (error, res) => {
+        if (error) {
+            console.error('Error deleting item:', error);
+            res.status(500).send('Error deleting item');
+
+        } else {
+            res.redirect('/');
+        }
+    })
+
+
+});
