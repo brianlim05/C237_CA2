38ab2903247edf1335fe82f3b4debc5a923e48bf
Tan Zhi Shan <24035177@myrp.edu.sg>
2025-07-30 12:25:39 +0800

    add and borrow

---
+++ routes/addItem.js
@@ -0,0 +1,23 @@
+app.get('/addItem', (req, res) => {
+    res.render('addItem', {user: req.session.user } ); 
+});
+
+app.post('/addItem', upload.single('image'),  (req, res) => {
+    const { name, tag, quantity, price, availability } = req.body;
+    let image;
+    if (req.file) {
+        image = req.file.filename;
+    } else {
+        image = null;
+    }
+
+    const sql = 'INSERT INTO items (itemName, itemTag, quantity, price, availability , image) VALUES (?,?,?, ?, ?, ?)';
+    connection.query(sql , [name, tag, quantity, price, availability , image], (error, results) => {
+        if (error) {
+            console.error("Error adding item:", error);
+            res.status(500).send('Error adding item');
+        } else {
+            res.redirect('/inventory');
+        }
+    });
+});
